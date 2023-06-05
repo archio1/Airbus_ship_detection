f31fef71f8c5e8e69312b20869a37e0b7a2c97a3
archio1 <arti123t@gmail.com>
2023-06-05 19:32:30 +0300

    add: - add few images and csv - add comments and typization

--- README.md
+++ README.md
@@ -10,13 +10,13 @@ The problem involves developing a model that can accurately identify and segment
 
 To tackle this challenge, the following approach has been used:
 
-1. **Semantic Segmentation Model**: The solution utilizes the U-Net architecture, a popular choice for image segmentation tasks. U-Net is known for its ability to capture detailed information and preserve spatial context, making it suitable for this task.
+1. **Python**: The entire solution is implemented using the Python programming language, taking advantage of its rich ecosystem of libraries and tools for data manipulation, image processing, and deep learning.
 
 2. **tf.keras**: The implementation of the model is done using `tf.keras`, a high-level deep learning API that allows for easy model development and training. It leverages the capabilities of TensorFlow while providing a user-friendly interface.
 
 3. **Dice Score**: The evaluation metric used for the model is the Dice coefficient, which measures the similarity between the predicted masks and the ground truth masks. The Dice coefficient is a commonly used metric for semantic segmentation tasks and provides a measure of segmentation accuracy.
 
-4. **Python**: The entire solution is implemented using the Python programming language, taking advantage of its rich ecosystem of libraries and tools for data manipulation, image processing, and deep learning.
+4. **Semantic Segmentation Model**: The solution utilizes the U-Net architecture, a popular choice for image segmentation tasks. U-Net is known for its ability to capture detailed information and preserve spatial context, making it suitable for this task.
 
 ## Project Structure
 
@@ -25,35 +25,33 @@ The repository contains the following files:
 - `exploratory_data_analysis.ipynb`: Jupyter Notebook for exploratory data analysis.
 - `inference.py`: Python script for performing inference and generating predicted masks.
 - `unet.py`: Python script containing the implementation of the U-Net architecture for the neural network.
-- `seg_model.h5`: Trained model file in the HDF5 format.
-- `seg_model_weights_best.h5`: Trained model weights file in the HDF5 format.
+- `model/`: Trained model and weights file in the HDF5 format.
 - `requirements.txt`: Text file listing the required dependencies for running the project.
 - `airbus-ship-detection/`: Directory containing the Airbus Ship Detection dataset.
 
 ## Running the Project
 To run the project, follow the instructions below:
 
-1. First of all create an environment
+0. kaggle competitions download -c airbus-ship-detection
+1. Create a virtual environment and install dependencies from requirements.txt
 2. To explore the dataset and perform data analysis, open `exploratory_data_analysis.ipynb` in Jupyter Notebook.
 
-3. For running inference on new images, execute the following command: 
-
-```python
-python inference.py --input 'full path to file 
-```
-
-4. To train and evaluate the Unet model, execute the command: 
+3. To train and evaluate the Unet model, execute the command: 
 ```python
 python unet.py
 ```
 Make sure you have the Airbus Ship Detection dataset available.
 
-5. Please note that Google Colab I couldn't use for training the model in this project. Because I ran into a problem of limited memory for using the datasetю
+4. For running inference on new images, execute the following command: 
+
+```python
+python inference.py --input  C:\Users\User\PycharmProjects\pythonProject\Project_check\Airbus_ship_detection\airbus-ship-detection\test_v2\00a3ab3cc.jpg
+```
 
 ## Conclusion
 
 The semantic segmentation model developed using the U-Net architecture and tf.keras provides a solution to the Airbus Ship Detection Challenge. By leveraging deep learning techniques and the powerful features of the U-Net model, accurate ship segmentation can be achieved in satellite images.
 
-The provided code and instructions enable further exploration and improvement of the model, allowing for experimentation with different architectures, hyperparameters, and data augmentation techniques.
-
-For more details, please refer to the individual code files and comments within the codebase.
+Please note that Google Colab I couldn't use for training the model in this project. Because I ran into a problem of limited memory for using the dataset.
+Model shows low accuracy. To improve the accuracy of model: increase the model's capacity, adjust the learning rate, try different loss functions, regularize the model, hyperparameter tuning.
+This project was created by Artem Tatarchuk. For any questions or additional information, please contact arti123t@gmail.com.
--- inference.py
+++ inference.py
@@ -3,71 +3,133 @@ import cv2
 import matplotlib.pyplot as plt
 from keras import models
 from keras.utils import custom_object_scope
-import keras.backend as K
 import argparse
 from pathlib import Path
+import keras.backend as K
+
+# from unet.py import dice_coef
+# can not import dice_coef from unet.py
 
 
-def dice_coef(y_true, y_pred, smooth = 1):
+def dice_coef(y_true, y_pred, smooth: float = 1.0):
+    """
+    Compute the Dice coefficient.
+
+    Args:
+        y_true (K.Tensor): True target tensor.
+        y_pred (K.Tensor): Predicted target tensor.
+        smooth (float): Smoothing factor.
+
+    Returns:
+        K.Tensor: Dice coefficient.
+    """
     intersection = K.sum(y_true * y_pred, axis=[1, 2, 3])
     union = K.sum(y_true, axis=[1, 2, 3]) + K.sum(y_pred, axis=[1, 2, 3])
-    return K.mean((2. * intersection + smooth) / (union + smooth), axis=0)
+    return K.mean((2.0 * intersection + smooth) / (union + smooth), axis=0)
 
-def load_model(model_path):
-    # Load the trained model
-     with custom_object_scope({'dice_coef': dice_coef}):
+
+def load_model(model_path: Path) -> models.Model:
+    """
+    Load a pre-trained model from the specified path.
+
+    Args:
+        model_path (Path): Path to the model file.
+
+    Returns:
+        model (Model): Loaded pre-trained model.
+        model (Model): Loaded pre-trained model.
+    """
+    with custom_object_scope({"dice_coef": dice_coef}):
         model = models.load_model(model_path)
-        return model
+    return model
 
-def preprocess_image(image_path):
-    # Load the test image
+
+def preprocess_image(image_path: str) -> np.ndarray:
+    """
+    Preprocess the image for inference.
+
+    Args:
+        image_path (str): Path to the image for prediction.
+
+    Returns:
+        test_image (np.ndarray): Preprocessed image.
+    """
     test_image = cv2.imread(image_path)
 
-    # Preprocess the test image
     test_image = cv2.cvtColor(test_image, cv2.COLOR_BGR2RGB)
     test_image = cv2.resize(test_image, (768, 768))
     test_image = test_image.astype(np.float32) / 255.0
 
-    # Reshape the test image for model input
     test_image = np.expand_dims(test_image, axis=0)
     return test_image
 
-def perform_inference(model, test_image):
-    # Perform inference
+
+def perform_inference(model: models.Model, test_image: np.ndarray) -> np.ndarray:
+    """
+    Perform inference on the image.
+
+    Args:
+        model (Model): Pre-trained model.
+        test_image (np.ndarray): Preprocessed image.
+
+    Returns:
+        predicted_mask (np.ndarray): Predicted mask.
+    """
     predicted_mask = model.predict(test_image)
-    predicted_mask = (predicted_mask.squeeze() > 0.5).astype(np.uint8)  # Threshold the predicted mask
+    predicted_mask = (predicted_mask.squeeze() > 0.5).astype(
+        np.uint8
+    )  # Threshold the predicted mask
     return predicted_mask
 
-def visualize_results(test_image, predicted_mask):
-    # Visualize the test image and the predicted mask
+
+def visualize_results(test_image: np.ndarray, predicted_mask: np.ndarray):
+    """
+    Visualize the results.
+
+    Args:
+        test_image (np.ndarray): Test image.
+        predicted_mask (np.ndarray): Predicted mask.
+    """
     fig, axes = plt.subplots(1, 2, figsize=(10, 5))
 
-    # Show the test image
     axes[0].imshow(test_image[0])
     axes[0].set_title("Test Image")
 
-    # Show the predicted mask
-    axes[1].imshow(predicted_mask, cmap='gray')
+    axes[1].imshow(predicted_mask, cmap="gray")
     axes[1].set_title("Predicted Mask")
 
     plt.tight_layout()
     plt.show()
 
+    if np.any(predicted_mask):
+        print("Ship is present in the image.")
+    else:
+        print("No ship detected in the image.")
+
+
 if __name__ == "__main__":
-    # Paths
     parser = argparse.ArgumentParser()
     parser.add_argument("--input", type=str, help="Path to the image for prediction")
     args = parser.parse_args()
-    model_path = Path('seg_model.h5')
+    model_path = Path("model/seg_model.h5")
+    if not model_path.exists():
+        raise FileNotFoundError(
+            "Model file not found. Please ensure 'seg_model.h5' is in the correct directory."
+        )
 
-    # Load the model
+    if args.input is None:
+        raise ValueError(
+            "Please provide the path to the image for prediction using the '--input' argument."
+        )
+    elif not Path(args.input).is_file():
+        raise FileNotFoundError(
+            "Input image file not found. Please ensure the provided path is correct."
+        )
+
     model = load_model(model_path)
 
-    # Preprocess the test image
     test_image = preprocess_image(args.input)
 
-    # Perform inference
     predicted_mask = perform_inference(model, test_image)
 
-    # Visualize the results
     visualize_results(test_image, predicted_mask)
